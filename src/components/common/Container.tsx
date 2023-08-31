3e006fd1d18ca39df67dc47d161e772ff85b47b1
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-09-01 01:17:01 +0900

    Design: Container 컴포넌트 mobile, tablet 반응형 설정

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -14,4 +14,12 @@ export default Container;
 const ConatainerLayout = styled.div`
   max-width: 1280px;
   margin: 0 auto;
+
+  @media screen and (max-width: 1023px) {
+    max-width: 768px;
+  }
+
+  @media screen and (max-width: 767px) {
+    max-width: 350px;
+  }
 `;
